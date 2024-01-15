edd19dfb7a5ef65ae5e692bb4ca230c993604ba5
BiswajitBehera23 <b.biswajitbehera2000@gmail.com>
2024-01-15 21:42:25 +0530

    array length

--- basic/array.java
+++ basic/array.java
@@ -1,5 +1,18 @@
-class test_array {
-   public static void main(String[] args) {
-     System.out.println("array")
-       }
+package basic;
+
+/**
+ * array
+ */
+public class array {
+
+  public static void main(String[] args) {
+    int array[] = new int[7];
+    for (int count = 0; count < 7; count++) {
+      array[count] = count + 1;
+    }
+    for (int count = 0; count < 7; count++) {
+      System.out.println("array["+count+"] = "+array[count]);
+    }
+  }
 }
+
