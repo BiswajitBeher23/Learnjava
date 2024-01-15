e9fec099fbb7540be8696daefc431edc03a8f4b5
BiswajitBehera23 <b.biswajitbehera2000@gmail.com>
2024-01-15 20:58:42 +0530

    test

---
+++ basic/Classobj.java
@@ -0,0 +1,27 @@
+package basic;
+
+public class Classobj {
+    
+    int age;
+    String name;
+    String address;
+    double salary;
+
+    public String getInfo() {
+       return(name +" is "+age+"years old and he is from"+address+". His salary is "+salary);
+    } 
+
+    public static void main(String[] args) {
+        Classobj tipun = new Classobj();
+        tipun.age = 23;
+        tipun.name = "biswajit";
+        tipun.address = "meduakul";
+        tipun.salary = 30000.00;
+        System.out.println(tipun.getInfo());
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ basic/biswa.java
@@ -0,0 +1,31 @@
+package basic;
+
+import java.util.Scanner;
+class tipu{
+    void test(){
+        Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+        int c = a+b;
+       int d = a-b;
+
+       if(a%2==0 && b%2==0){
+        System.out.println(c);
+        
+
+       }
+       else{
+        System.out.println(d);
+       }
+    }
+}
+
+class biswa {
+
+    public static void main(String[] args) {
+        tipu obj = new tipu();
+        obj. test();
+       
+       
+    }
+}
