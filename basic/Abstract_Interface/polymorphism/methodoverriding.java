2c48eac93199ecf8265efd2605eb881e8798fec6
BiswajitBehera23 <b.biswajitbehera2000@gmail.com>
2024-01-24 20:42:02 +0530

    updated

---
+++ basic/Abstract_Interface/polymorphism/methodoverriding.java
@@ -0,0 +1,20 @@
+package Basic.Abstract_Interface.polymorphism;
+
+public class methodoverriding {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
+
+// class Parent {
+//     void show(){
+//         System.out.println("Parent's tv show()");
+//     }
+// }
+
+// class Child extends Parent {
+//      void show(){
+//         System.out.println("child's tv show");
+//     }
+// }
