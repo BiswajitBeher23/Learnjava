bfd87402d39cbb7c37e70931a5b2713c0d777a8f
Debasish Brahma <dbrahma4@outlook.com>
2024-02-01 21:19:48 +0530

    new file added

---
+++ interm/Exceptionhanding/CheckClass.java
@@ -0,0 +1,31 @@
+package interm.Exceptionhanding;
+
+import java.lang.reflect.Method;
+
+class MyClass {
+    void m2() {
+        System.out.println("hello");
+    }
+}
+
+public class CheckClass {
+
+    public static void main(String[] args) {
+        String className = "MyClass";
+
+        try {
+            Class<?> myClass = Class.forName(className);
+            System.out.println("The class " + className + " is present.");
+
+            // Get the methods of the class.
+            Method[] methods = myClass.getMethods();
+
+            // Print the methods of the class.
+            for (Method method : methods) {
+                System.out.println(method.getName());
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("The class " + className + " is not present.");
+        }
+    }
+}
